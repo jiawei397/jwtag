a59cabd00b9fe3baa7a9a7b2f7459bcac9045f8a
jia wei <jw397@126.com>
2021-06-22 19:26:49 +0800

    feat: init

---
+++ REAEME.md
@@ -0,0 +1,3 @@
+# tag
+
+找到package.json中版本号，git打标签并推送到服务器。
---
+++ index.js
@@ -0,0 +1,25 @@
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+let version;
+try {
+    const pkg = fs.readFileSync('package.json', {
+        encoding: 'utf-8'
+    });
+    version = JSON.parse(pkg).version;
+} catch (e) {
+    console.error(`未找到package.json文件`);
+    process.exit(1);
+}
+
+const tasks = [
+    `git tag -a ${version} -m "v-${version}"`,
+    `git push origin ${version}`
+];
+
+tasks.forEach((str) => {
+    execSync(str);
+    console.log(`任务【${str}】完成！`);
+});
+
+console.log('end');
